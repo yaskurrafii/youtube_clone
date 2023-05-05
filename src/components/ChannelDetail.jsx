62a412a7f2598c276ea7d959059d9e404c929a32
muhammad rafii <myasykurrafii@lndata.com>
2023-05-05 16:48:16 +0700

    feat: :sparkles: adding channel page

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import { Videos, ChannelCard } from ".";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const ChannelDetail = () => {
-  return <div>ChannelDetail</div>;
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setvideos] = useState([]);
+  const { id } = useParams();
+  console.log(channelDetail, videos);
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=${id}`).then((data) =>
+      setChannelDetail(data?.items[0])
+    );
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then(
+      (data) => setvideos(data?.items)
+    );
+  }, [id]);
+
+  return <div>{id}</div>;
 };
 
 export default ChannelDetail;
